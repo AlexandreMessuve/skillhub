92f6beb05f51c17f90288a5a9c27ecb8b1859dd4
Alexandre Messuve <alexandre.messuves@gmail.com>
2025-07-01 16:22:19 +0200

    add user entity

---
+++ server/authentication-service/src/main/java/com/skillhub/entity/UserInfo.java
@@ -0,0 +1,62 @@
+package com.skillhub.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class UserInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID userId;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private Role role = Role.USER;
+
+    @Column(name = "phone_number", unique = true)
+    private String phoneNumber;
+
+    @Column(name = "verification_token", nullable = true)
+    private String verificationToken;
+
+    private boolean verified;
+
+    @Column(name = "2fa_enabled")
+    private boolean doubleAuthEnabled;
+
+    @Column(name = "public_key")
+    private String publicKey;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
