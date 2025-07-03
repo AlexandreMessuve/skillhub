51a200803744b7018f92766d0383904a81163f21
Alexandre Messuve <alexandre.messuves@gmail.com>
2025-07-03 13:05:33 +0200

    add spring validation on the attributes

--- server/authentication-service/src/main/java/com/skillhub/dto/AccountCreationRequest.java
+++ server/authentication-service/src/main/java/com/skillhub/dto/AccountCreationRequest.java
@@ -1,17 +1,35 @@
 package com.skillhub.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
+import org.springframework.validation.annotation.Validated;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
+@Validated
 public class AccountCreationRequest {
+    @NotBlank(message = "Email cannot be null")
+    @Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$", message = "Invalid email format")
     private String email;
+
+    @NotBlank(message = "Password cannot be null")
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "Password must be at least 8 characters long, contain at least one uppercase letter," +
+                    "one lowercase letter, one digit, and one special character.")
     private String password;
+
+    @NotBlank(message = "Last name cannot be null")
     private String lastname;
+
+    @NotBlank(message = "First name cannot be null")
     private String firstname;
+
+    @NotBlank(message = "Phone number cannot be null")
+    @Pattern(regexp = "^\\+?[0-9]{10,15}$", message = "Invalid phone number format")
     private String phone;
 
 }
--- server/authentication-service/src/main/java/com/skillhub/dto/AuthenticationRequest.java
+++ server/authentication-service/src/main/java/com/skillhub/dto/AuthenticationRequest.java
@@ -1,14 +1,26 @@
 package com.skillhub.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
+import org.springframework.validation.annotation.Validated;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Validated
 public class AuthenticationRequest {
 
+    @NotBlank(message = "Email cannot be null")
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
+            message = "Email must be a valid email address.")
     private String email;
+
+    @NotBlank(message = "Password cannot be null")
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "Password must be at least 8 characters long, contain at least one uppercase letter," +
+                    "one lowercase letter, one digit, and one special character.")
     private String password;
 }
