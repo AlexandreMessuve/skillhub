99fa2c9f40ebf69581daf4faf3d677d4b08d8171
Alexandre Messuve <alexandre.messuves@gmail.com>
2025-07-03 10:44:14 +0200

    update CustomUserService

--- server/authentication-service/src/main/java/com/skillhub/service/CustomUserDetailsService.java
+++ server/authentication-service/src/main/java/com/skillhub/service/CustomUserDetailsService.java
@@ -2,10 +2,10 @@ package com.skillhub.service;
 
 import com.skillhub.repository.UserInfoRepository;
 import com.skillhub.entity.UserInfo;
+import com.skillhub.security.CustomUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,11 +13,13 @@ import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserInfoRepository userInfoRepository;
+
 
     @Autowired
     public CustomUserDetailsService(UserInfoRepository userInfoRepository) {
@@ -29,9 +31,14 @@ public class CustomUserDetailsService implements UserDetailsService {
         UserInfo userInfo = userInfoRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found" + email));
 
-        GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + userInfo.getRole().name());
-        List<GrantedAuthority> authorities = Collections.singletonList(authority);
+        return new CustomUserDetails(
+                userInfo.getUserId(),
+                userInfo.getEmail(),
+                userInfo.getPassword(),
+                userInfo.getRole(),
+                userInfo.getFirstName(),
+                userInfo.getLastName()
+        );
 
-        return new User(userInfo.getEmail(), userInfo.getPassword(), authorities);
     }
 }
