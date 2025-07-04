0e2dfe43dbc479a8b4ce59d78af17d2b5471cff4
Alexandre Messuve <alexandre.messuves@gmail.com>
2025-07-04 03:07:17 +0200

    add two factor service, verify generate 2fa code & qr code

---
+++ server/authentication-service/src/main/java/com/skillhub/service/TwoFactorAuthService.java
@@ -0,0 +1,56 @@
+package com.skillhub.service;
+
+import dev.samstevens.totp.code.*;
+import dev.samstevens.totp.exceptions.QrGenerationException;
+import dev.samstevens.totp.qr.QrData;
+import dev.samstevens.totp.qr.QrGenerator;
+import dev.samstevens.totp.qr.ZxingPngQrGenerator;
+import dev.samstevens.totp.secret.DefaultSecretGenerator;
+import dev.samstevens.totp.secret.SecretGenerator;
+import dev.samstevens.totp.time.SystemTimeProvider;
+import dev.samstevens.totp.time.TimeProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import static dev.samstevens.totp.util.Utils.getDataUriForImage;
+@Service
+public class TwoFactorAuthService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TwoFactorAuthService.class);
+
+    public String generateNewSecret(){
+        SecretGenerator secretGenerator = new DefaultSecretGenerator();
+        return secretGenerator.generate();
+    }
+
+    public String getQrCodeUri(String secret, String email) throws RuntimeException{
+        QrData data = new QrData.Builder()
+                .label(email)
+                .secret(secret)
+                .issuer("SkillHub")
+                .algorithm(HashingAlgorithm.SHA1)
+                .digits(6)
+                .period(30)
+                .build();
+
+        QrGenerator qrGenerator = new ZxingPngQrGenerator();
+        try {
+            byte[] qrCode = qrGenerator.generate(data);
+            return getDataUriForImage(qrCode, qrGenerator.getImageMimeType());
+        } catch (QrGenerationException e) {
+            LOG.error("Error generating QR code for secret: {}", secret, e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean isValidCode(String secret, String code) {
+        TimeProvider timeProvider = new SystemTimeProvider();
+        CodeGenerator codeGenerator = new DefaultCodeGenerator(HashingAlgorithm.SHA1, 6);
+        DefaultCodeVerifier verifier = new DefaultCodeVerifier(codeGenerator, timeProvider);
+        verifier.setAllowedTimePeriodDiscrepancy(1);
+
+        LOG.info("Code validation for secret {}", secret);
+        return verifier.isValidCode(secret, code);
+    }
+}
